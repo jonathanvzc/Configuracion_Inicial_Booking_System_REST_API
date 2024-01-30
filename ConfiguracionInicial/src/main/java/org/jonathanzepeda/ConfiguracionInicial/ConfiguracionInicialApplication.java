034e25f8b2d6b808735494e8c66cac9f58b31cef
jonathanvzc <jonathanvzc@gmail.com>
2024-01-29 20:12:19 -0600

    Configuración inicial del proyecto

---
+++ ConfiguracionInicial/src/main/java/org/jonathanzepeda/ConfiguracionInicial/ConfiguracionInicialApplication.java
@@ -0,0 +1,14 @@
+package org.jonathanzepeda.ConfiguracionInicial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfiguracionInicialApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ConfiguracionInicialApplication.class, args);
+	}
+
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Configuracion_Inicial_Booking_System_REST_API
- Proyecto Integrador: configuración inicial Booking System REST API.
